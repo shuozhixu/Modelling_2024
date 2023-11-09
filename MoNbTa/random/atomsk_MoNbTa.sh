268f15a5f4cef5d67b66899d7bb3e5a9ce5811de
shuozhixu <shuozhixu@gmail.com>
2023-11-09 14:45:52 -0600

    Update atomsk_MoNbTa.sh

--- MoNbTa/random/atomsk_MoNbTa.sh
+++ MoNbTa/random/atomsk_MoNbTa.sh
@@ -6,9 +6,9 @@ a=3.243
 
 atomsk --create bcc $a Mo orient [11-2] [111] [1-10] -duplicate 10 46 14 Mo.cfg
 
-atomsk Mo.cfg -select random 12880 Mo -sub Mo Nb MoNb.cfg
+atomsk Mo.cfg -select random 33.33% Mo -sub Mo Nb MoNb.cfg
 
-atomsk MoNb.cfg -select random 12880 Mo -sub Mo Ta data.MoNbTa_random.cfg lmp
+atomsk MoNb.cfg -select random 50% Mo -sub Mo Ta data.MoNbTa_random.cfg lmp
 
 mv data.MoNbTa_random.lmp data.MoNbTa_random
 
